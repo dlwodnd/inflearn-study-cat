0330b2d1bf6a57ad312c387aaccc16c6617a7510
leejaewoong <leejaewoong300@gmail.com>
2024-03-25 17:09:54 +0900

    asddas

---
+++ cat-jjal-maker-cra/src/App.js
@@ -0,0 +1,144 @@
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import Title from './components/title.js';
+const jsonLocalStorage = {
+  setItem: (key, value) => {
+    localStorage.setItem(key, JSON.stringify(value));
+  },
+  getItem: (key) => {
+    return JSON.parse(localStorage.getItem(key));
+  },
+};
+const fetchCat = async (text) => {
+  const OPEN_API_DOMAIN = "https://cataas.com";
+  const response = await fetch(
+    `${OPEN_API_DOMAIN}/cat/says/${text}?json=true`
+  );
+  const responseJson = await response.json();
+  return `${OPEN_API_DOMAIN}/cat/${responseJson._id}/says/${text}`; // NOTE: API 스펙 변경으로 강의 영상과 다른 URL로 변경했습니다.
+};
+
+function CatItem(props) {
+  return (
+    <li>
+      <img src={props.img} style={{ width: "150px" }} />
+    </li>
+  );
+}
+
+function Favorites({ favorites }) {
+  if (favorites.length === 0) {
+    return <div>사진위 하트르 눌러 고양이 사진을 저장해 봐요!</div>;
+  }
+  return (
+    <ul className="favorites">
+      {favorites.map((cat) => (
+        <CatItem img={cat} key={cat} />
+      ))}
+    </ul>
+  );
+}
+
+const MainCard = ({ img, onHeartClick , alreadyFav }) => {
+  const heartIcon = alreadyFav ? "❤️" : "🤍";
+  return (
+    <div className="main-card">
+      <img src={img} alt="고양이" width="400" />
+      <button onClick={onHeartClick}>{heartIcon}</button>
+    </div>
+  );
+};
+
+
+
+const Form = ({ updateMainCat }) => {
+  const includesHangul = (text) => /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/i.test(text);
+  const [value, setValue] = React.useState("");
+  const [errorMsg, setErrorMsg] = React.useState("");
+  function handleInputChange(event) {
+    const uservalue = event.target.value;
+    setErrorMsg("");
+    if (includesHangul(uservalue)) {
+      setErrorMsg("한글은 입력할 수 없습니다.");
+      return;
+    }
+    setValue(uservalue.toUpperCase());
+  }
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    setErrorMsg("");
+    if (value === "") {
+      setErrorMsg("값을 입력해주세요");
+      return;
+    }
+    updateMainCat(value);
+  }
+  return (
+    <form onSubmit={handleFormSubmit}>
+      <input
+        type="text"
+        placeholder="영어 대사를 입력해주세요"
+        name="name"
+        onChange={handleInputChange}
+        value={value}
+      />
+      <button type="submit">생성</button>
+      <p style={{ color: "red" }}>{errorMsg}</p>
+    </form>
+  );
+};
+
+const App = () => {
+  const CAT1 = "https://cataas.com/cat/HSENVDU4ZMqy7KQ0/says/react";
+  const CAT2 = "https://cataas.com/cat/BxqL2EjFmtxDkAm2/says/inflearn";
+  const CAT3 = "https://cataas.com/cat/18MD6byVC1yKGpXp/says/JavaScript";
+  const [counter ,setCounter] = React.useState(() => {
+    return jsonLocalStorage.getItem("counter") || null;
+  })
+  const [catImg, setCatImg] = React.useState(CAT1);
+  const [favorites, setFavorites] = React.useState(() => {
+    return jsonLocalStorage.getItem("favorites") || []}
+  );
+
+  const alreadyFav = favorites.includes(catImg);
+
+  async function setInitialCat() {
+    const newCat = await fetchCat("First Cat");
+    console.log(newCat);
+    setCatImg(newCat);
+  }
+  React.useEffect(() => {
+    setInitialCat();
+  }, []);
+  // setInitialCat();
+  const countNum = counter === null ? null : counter + '번째 ';
+
+  async function updateMainCat(value) {
+    const newCat = await fetchCat(value);
+    
+    setCatImg(newCat);
+    setCounter((prev) => {
+      const nextCount = prev + 1;
+      jsonLocalStorage.setItem("counter", nextCount);
+      return nextCount;
+    });
+    
+  }
+  function handleClick() {
+    const nextFavorites = [...favorites, catImg];
+    setFavorites(nextFavorites);
+    jsonLocalStorage.setItem("favorites", nextFavorites);
+  }
+
+  return (
+    <div>
+      <Title>{countNum}고양이 가라사대</Title>
+      <Form updateMainCat={updateMainCat} />
+      <MainCard img={catImg} onHeartClick={handleClick} alreadyFav={alreadyFav}/>
+      <Favorites favorites={favorites} />
+    </div>
+  );
+};
+
+export default App;
